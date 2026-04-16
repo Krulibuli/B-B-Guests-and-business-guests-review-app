f9ef2829d1de873aa199405a6bc97c4c292e3f91
Gedis110 <gedissim110@gmail.com>
2026-04-16 12:29:00 +0200

    fixed maybe?

--- script/srctipt.js
+++ script/srctipt.js
@@ -5,7 +5,7 @@ star4 = document.querySelector('.very-happy');
 let i=0
 star1.addEventListener("click", ()=>{i=1 
 if (i==1) {
-    star1.style='color:red;';
+    star1.style='color:red; font-size: 600%;';
     star2.style='color:gray;';
     star3.style='color:gray;';
     star4.style='color:gray;';
@@ -13,7 +13,7 @@ if (i==1) {
 star2.addEventListener("click", ()=>{i=2 
 if (i==2) {
     star1.style='color:gray;';
-    star2.style='color:pink;';
+    star2.style='color:pink; font-size: 600%;';
     star3.style='color:gray;';
     star4.style='color:gray;';
 }})
@@ -21,7 +21,7 @@ star3.addEventListener("click", ()=>{i=3
 if (i==3) {
     star1.style='color:gray;';
     star2.style='color:gray;';
-    star3.style='color:gold;';
+    star3.style='color:gold;  font-size: 600%;';
     star4.style='color:gray;';
 }})
 star4.addEventListener("click", ()=>{i=4 
@@ -29,7 +29,7 @@ if (i==4) {
     star1.style='color:gray;';
     star2.style='color:gray;';
     star3.style='color:gray;';
-    star4.style='color:green;';
+    star4.style='color:green;  font-size: 600%;';
 }})
 
 
